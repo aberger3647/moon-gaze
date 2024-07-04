266ac923e1db2254a567282f1fee0a7bc78a38ca
aberger3647 <112574259+aberger3647@users.noreply.github.com>
2024-07-04 17:17:20 -0500

    open browser, enter austin, navigate, screenshot, close browser

--- moon-gaze/scrape/index.js
+++ moon-gaze/scrape/index.js
@@ -12,14 +12,12 @@ const path = require('path');
         await page.goto(
             "https://darksky.org/what-we-do/international-dark-sky-places/all-places/"
         );
-        // await page.screenshot({ path: 'page.png' });
-
-        const screenshotPath = path.join(process.cwd(), 'example_screenshot.png');
+        await page.getByLabel('Keywords, name, etc.').fill('Austin');
+        await page.locator('i.facetwp-icon').click();
+        const screenshotPath = path.join(process.cwd(), 'screenshot.png');
         console.log('Taking screenshot...');
-        await page.screenshot({ path: screenshotPath });
+        await page.screenshot({ path: screenshotPath, fullPage: true });
         console.log(`Screenshot saved to: ${screenshotPath}`);
-        // await page.getByLabel('Keywords, name, etc.').fill('Austin');
-        // await page.getByRole('i', { class: 'facetwp-icon' }).click();
 
   } catch (error) {
     console.error('An error occurred:', error);
@@ -32,11 +30,5 @@ const path = require('path');
     console.log('Script finished');
   }
 
-
-// // Keep the Node.js process alive
-// process.on('unhandledRejection', (reason, promise) => {
-//   console.log('Unhandled Rejection at:', promise, 'reason:', reason);
-//   // Application specific logging, throwing an error, or other logic here
-// });
 
 })().catch(error => console.error('Unhandled error in main function: ', error))
