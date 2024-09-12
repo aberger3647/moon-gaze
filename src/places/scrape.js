653376190b347aa44083060aa232444b5fd5a940
aberger3647 <112574259+aberger3647@users.noreply.github.com>
2024-09-12 16:24:37 -0500

    comment out add data to db

--- src/places/scrape.js
+++ src/places/scrape.js
@@ -1,1 +1,31 @@
 import places from "./places.json";
+const {firefox} = require("playwright");
+const path = require("path");
+
+const scrapeData = async () => {
+  let browser;
+  try {
+    console.log("Attempting to launch Firefox");
+    browser = await firefox.launch({
+      headless: false,
+    });
+    console.log("Launch successful");
+
+    const page = await browser.newPage();
+    await page.goto(
+      "https://darksky.org/places/albanya-dark-sky-park/",
+      { waitUntil: "networkidle", timeout: 60000, }
+    );
+    console.log("Page loaded");
+} catch (error) {
+    console.error("An error occurred:", error);
+  } finally {
+    if (browser) {
+      console.log("Closing browser...");
+      await browser.close();
+      console.log("Browser closed");
+    }
+    console.log("Script finished");
+  }
+};
+scrapeData();
