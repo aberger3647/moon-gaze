4e911dc5ba2ae7227a27d99cf46970eac9ff7f20
aberger3647 <112574259+aberger3647@users.noreply.github.com>
2024-09-12 21:34:20 -0500

    get coords from page

--- src/places/scrape.js
+++ src/places/scrape.js
@@ -1,5 +1,5 @@
-import places from "./places.json";
-const {firefox} = require("playwright");
+const places = require("./places.json");
+const { firefox } = require("playwright");
 const path = require("path");
 
 const scrapeData = async () => {
@@ -12,12 +12,20 @@ const scrapeData = async () => {
     console.log("Launch successful");
 
     const page = await browser.newPage();
-    await page.goto(
-      "https://darksky.org/places/albanya-dark-sky-park/",
-      { waitUntil: "networkidle", timeout: 60000, }
-    );
+    await page.goto("https://darksky.org/places/albanya-dark-sky-park/", {
+      waitUntil: "networkidle",
+      timeout: 60000,
+    });
     console.log("Page loaded");
-} catch (error) {
+
+    const weatherHeader = page.locator("h3").filter({ hasText: 'Weather' })
+    const weatherContent = await weatherHeader.locator(' + p').textContent();
+    const regex = /\(\d+(\.\d+)?,\s*\d+(\.\d+)?\)/;
+    const match = weatherContent.match(regex)
+    const coords = match[0]
+    console.log(coords);
+
+  } catch (error) {
     console.error("An error occurred:", error);
   } finally {
     if (browser) {
