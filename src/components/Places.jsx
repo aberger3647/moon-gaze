08f2babcda0107c262e3f69dce9bcf09c81ffa13
aberger3647 <112574259+aberger3647@users.noreply.github.com>
2025-01-21 21:46:06 -0600

    places render in order

--- src/components/Places.jsx
+++ src/components/Places.jsx
@@ -1,14 +1,34 @@
 import { Miles } from ".";
 
-export const Places = ({location}) => {
-    return(
-      <>
-      <h2>Dark Sky Places</h2>
-      <p>Within <span><Miles /></span> miles of <span>{location}</span></p>
+export const Places = ({ location, sortedPlaces, userCoords }) => {
+  console.log("sortedPlaces", sortedPlaces);
+  const nearbyPlaces = sortedPlaces.filter(place => {
+    return place.distance < 150;
+  });
 
-      <h4>Dripping Springs</h4>
-      <p>Community</p>
-    </>  
-    )
-  };
-  
+  console.log("nearbyPlaces", nearbyPlaces);
+
+  return (
+    <>
+      <h2>Dark Sky Places</h2>
+      <p>
+        Within{" "}
+        <span>
+          <Miles />
+        </span>{" "}
+        miles of <span>{location}</span>
+      </p>
+
+      {nearbyPlaces.map((place, index) => (
+        <>
+        <div key={index} className='place'>
+          <p>{place.placeName}</p>
+          <p>{place.category}</p>
+          <p>{`Distance: ${place.distance.toFixed(1)} miles`}</p>
+        </div>
+        <hr/>
+        </>
+      ))}
+    </>
+  );
+};
